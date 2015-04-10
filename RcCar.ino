04e42cb9e200ca0a45d35b0b783057e06671f794
Adesh Shah <adesh.shah.28@gmail.com>
2015-04-10 09:42:11 +0530

    speed logic added

--- RcCar.ino
+++ RcCar.ino
@@ -1,14 +1,11 @@
 /* 
  //contineous unless stop command
- 1 -> forward
- 2 -> reverse
- 3 -> left
- 4 -> right
- 0 -> stop
- 
- //once set
- 5 -> fast speed
- 6 -> slow speed
+ w -> forward
+ x -> reverse
+ a -> left
+ d -> right
+ s -> stop
+ 1:9 -> speed
  
  */
 #include <AFMotor.h>
@@ -21,7 +18,7 @@ AF_DCMotor motorRL(3);
 AF_DCMotor motorRR(4);
 
 char state;
-int s_speed = 240;
+int s_speed = 255;
 int iteration = 0;
 
 void setup() {
@@ -44,26 +41,54 @@ void loop() {
   //  state = Serial.read();
   //  BTSerial.print(state);
   //}
+  
   if(BTSerial.available() > 0){     
     state = BTSerial.read();
     //Serial.print(state);
     iteration = 0;
   }  
   
-  if (state == '0') {
+  if (state == 's') {
     stopALL();
   }
-  else if (state == '1') {
+  else if (state == 'w') {
     forward();
   }
-  else if (state == '2') {
+  else if (state == 'x') {
     backward();
   }
-  else if (state == '3') {
+  else if (state == 'a') {
     left();
   }
-  else if (state == '4') {
+  else if (state == 'd') {
     right();
+  }
+  else if (state == '1') {
+    set_speed(map(1, 1, 9, 0, 255));
+  }
+  else if (state == '2') {
+    set_speed(map(2, 1, 9, 0, 255));
+  }
+  else if (state == '3') {
+    set_speed(map(3, 1, 9, 0, 255));
+  }
+  else if (state == '4') {
+    set_speed(map(4, 1, 9, 0, 255));
+  }
+  else if (state == '5') {
+    set_speed(map(5, 1, 9, 0, 255));
+  }
+  else if (state == '6') {
+    set_speed(map(6, 1, 9, 0, 255));
+  }
+  else if (state == '7') {
+    set_speed(map(7, 1, 9, 0, 255));
+  }
+  else if (state == '8') {
+    set_speed(map(8, 1, 9, 0, 255));
+  }
+  else if (state == '9') {
+    set_speed(map(9, 1, 9, 0, 255));
   }
 
 }
