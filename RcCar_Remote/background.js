d6ee30836fecd84c19a0d98d608244005d4f46ff
Adesh Shah <adesh.shah.28@gmail.com>
2015-04-10 09:46:44 +0530

    bug fix

--- RcCar_Remote/background.js
+++ RcCar_Remote/background.js
@@ -1,74 +1,15 @@
-/* CSS file placeholder. */
-html,body,a{
-  cursor:crosshair;
-  margin:0px;
-  padding:0px;
-background: #222;
-}
+/**
+ * Listens for the app launching, then creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
 
-#remote{
-  padding:0px;
-  margin:0px;
-  border:0px Solid #dedede;
-  width:300px;
-  height:300px;
-}
-
-#remote #control{
-  font-size:85%;
-  height:150px;
-  width:150px;
-  position:absolute;
-  top:175px;
-  left:175px;
-  background: #2b2b2b;
-  color:white;
-  border-radius: 10px 10px 10px 10px;
-}
-
-select{
-  font-size:85%;  
-   background: #2b2b2b;
-  color:white; 
-}
-
-#remote .btn {
-  font-size:150%;
-  cursor:crosshair;
-  height:150px;
-  width:150px;
-  border:0px;
-  background: #57fa78;
-  color: #ffffff;
-  text-align:center;
-  text-decoration: none;
-}
-
-#remote .btn:hover {
-  text-decoration: none;
-}
-
-#remote #f{
-  position:absolute;
-  top:0px;
-  left:175px;
-  border-radius: 87px 87px 0px 0px;
-}
-#remote #b{
-  position:absolute;
-  bottom:0px;
-  left:175px;
-  border-radius: 0px 0px 87px 87px;
-}
-#remote #l{
-  position:absolute;
-  left:0px;
-  top:175px;
-  border-radius: 87px 0px 0px 87px;
-}
-#remote #r{
-  position:absolute;
-  right:0px;
-  top:175px;
-  border-radius: 0px 87px 87px 0px;
-}
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    'bounds': {
+      'width': 500,
+      'height':500
+    }
+  });
+});
